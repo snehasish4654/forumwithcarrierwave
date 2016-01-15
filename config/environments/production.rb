82bb404099e5e57c16f13e7ce6d5328464a09869
snehasish <findmyworld2k15@gmail.com>
2016-01-14 18:36:09 -0800

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address  => 'smtp.gmail.com' ,
-  :port     => '587' ,
+  :port     => '25' ,
   :authentication => :plain ,
   :user_name    => ENV['findmyworld2k15@gmail.com'],
   :password     => ENV['mynameuskhan'],
