b135a1b0fd06b02d16dbf964689301463954254b
snehasish <findmyworld2k15@gmail.com>
2016-01-14 18:10:17 -0800

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'myapp.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'hidden-spire-1398.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
