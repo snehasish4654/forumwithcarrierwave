aa875e7a88d1113ef7d5289f471c02ced9f25670
snehasish <findmyworld2k15@gmail.com>
2016-01-16 16:50:54 -0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   #resources :users 
-  resources :users
-  root to:'users#index'
+  
+  
   devise_for :models
+  root to: "users#index"
+  resources :users
   
 
 
